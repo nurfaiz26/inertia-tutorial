71d50f8591ff8d1fe996159f68c4ec9642c0bf8b
nurfaiz26 <mnurfaiz26@gmail.com>
2024-10-30 15:24:11 +0700

    part3: layouting

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,13 @@
-export default function Home({ name }) {
+// import Layout from "./Layout/Layout";
+
+const Home = ({ name }) => {
     return (
         <>
-            <h1 className="mt-10 text-center text-xl font-bold">Hello {name}</h1>
+            <h1 className="mt-10 text-center text-xl font-bold">Hello {name}!</h1>
         </>
     );
 }
+
+// Home.layout = page => <Layout children={page} />
+
+export default Home;
---
+++ resources/js/Pages/Layout/Layout.jsx
@@ -0,0 +1,25 @@
+import { Link } from "@inertiajs/react";
+import React from "react";
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <header>
+                <nav className="flex w-full justify-between items-center bg-indigo-900 px-12 py-2">
+                    <Link className="text-white text-lg font-semibold py-2 px-4 hover:bg-indigo-700 rounded-md" href="/">Home</Link>
+                    <Link className="text-white text-lg font-semibold py-2 px-4 hover:bg-indigo-700 rounded-md" href="/create">Create</Link>
+                </nav>
+            </header>
+
+            <main>
+                {children}
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default Layout;

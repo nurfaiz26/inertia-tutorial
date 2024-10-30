caaa55d8f476b84e96d8152dbf378679a8939f44
nurfaiz26 <mnurfaiz26@gmail.com>
2024-10-30 15:32:19 +0700

    part4: scroll position after click a <Link> to stay at its position

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,13 +1,15 @@
 // import Layout from "./Layout/Layout";
 
+import { Link } from "@inertiajs/react";
+
 const Home = ({ name }) => {
     return (
         <>
             <h1 className="mt-10 text-center text-xl font-bold">Hello {name}!</h1>
+
+            <Link preserveScroll href="/" className="block mt-[1000px] mb-10 text-xl font-bold text-center">{new Date().toLocaleTimeString()}</Link>
         </>
     );
 }
-
-// Home.layout = page => <Layout children={page} />
 
 export default Home;
